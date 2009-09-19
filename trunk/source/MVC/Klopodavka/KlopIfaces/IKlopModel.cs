17dfb3cee23548712a1d9da3c9f6d573fa086b9e
naxah1 <naxah1@36d63d38-9f94-11de-a33f-1de61d9b9616>
2009-09-19 16:03:42 +0000

    New solution to implement MVC pattern

---
+++ trunk/source/MVC/Klopodavka/KlopIfaces/IKlopModel.cs
@@ -0,0 +1,68 @@
+#region Usings
+
+using System.Collections.Generic;
+
+#endregion
+
+namespace KlopIfaces
+{
+   /// <summary>
+   /// Defines Model part of MVC pattern
+   /// </summary>
+   public interface IKlopModel
+   {
+      /// <summary>
+      /// Gets or sets the width of the field.
+      /// </summary>
+      /// <value>The width of the field.</value>
+      int FieldWidth { get; }
+
+      /// <summary>
+      /// Gets the height of the field.
+      /// </summary>
+      /// <value>The height of the field.</value>
+      int FieldHeight { get; }
+
+      /// <summary>
+      /// Gets the players.
+      /// </summary>
+      /// <value>The players.</value>
+      ICollection<IKlopPlayer> Players { get; }
+
+      /// <summary>
+      /// Gets the current player.
+      /// </summary>
+      /// <value>The current player.</value>
+      IKlopPlayer CurrentPlayer { get; }
+
+      /// <summary>
+      /// Gets the available klop count for each turn
+      /// </summary>
+      /// <value>The length of the turn.</value>
+      int TurnLength { get; }
+
+      /// <summary>
+      /// Gets the remaining klops.
+      /// </summary>
+      /// <value>The remaining klops.</value>
+      int RemainingKlops { get; }
+
+      /// <summary>
+      /// Gets the <see cref="KlopIfaces.IKlopCell"/> with the specified position
+      /// </summary>
+      /// <value></value>
+      IKlopCell this[int x, int y] { get; }
+
+      /// <summary>
+      /// Resets the game to initial state
+      /// </summary>
+      void Reset();
+
+      /// <summary>
+      /// Makes the turn - put klop to specified position
+      /// </summary>
+      /// <param name="x">The x.</param>
+      /// <param name="y">The y.</param>
+      void MakeTurn(int x, int y);
+   }
+}
