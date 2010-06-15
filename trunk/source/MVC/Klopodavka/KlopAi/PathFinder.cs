9cfb9dec74f503d2568a8c5030b25a652f9925d4
nAXAH1 <nAXAH1@36d63d38-9f94-11de-a33f-1de61d9b9616>
2010-06-15 11:39:28 +0000

    KlopAi draft (adapted from old version - needs refactoring)

---
+++ trunk/source/MVC/Klopodavka/KlopAi/PathFinder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Clops.Ai.Algo;
+using KlopIfaces;
+
+namespace KlopAi
+{
+    class ClopPathFinder
+    {
+        private readonly Node[,] Field;
+
+        public ClopPathFinder(IKlopModel model)
+        {
+            Field = new Node[model.FieldWidth, model.FieldHeight];
+           foreach (IKlopCell cell in model.Cells)
+           {
+              Field[cell.X, cell.Y] = new Node(cell.X, cell.Y);
+           }
+            //for (int i = 0; i <= model.FieldWidth; i++)
+            //    for (int j = 0; j <= model.FieldHeight; j++)
+        }
+
+        private static double dist(Node n1, Node n2)
+        {
+           int dx = n1.px - n2.px;
+           int dy = n1.py - n2.py;
+           return Math.Sqrt(dx * dx + dy * dy);
+        }
+
+        private Node nodexy(int x, int y)
+        {
+            if ((x >= 0) & (x < Field.GetLength(0)) & (y >= 0) & (y < Field.GetLength(1)))
+                return Field[x, y];
+            else
+                return null;
+        }
+
+        public Node FindPath(int sx, int sy, int fx, int fy)
+        {
+            return (new a_star()).FindPath(nodexy(sx, sy), nodexy(fx, fy), dist, nodexy);
+        }
+
+    }
+}
---
+++ trunk/source/MVC/Klopodavka/KlopAi/algo/a_star.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections;
+using Clops.Ifaces;
+
+namespace Clops.Ai.Algo
+{
+    /// <summary>
+    /// Summary description for a_star.
+    /// </summary>
+    public class a_star
+    {
+        private readonly Priority_queue Open = new Priority_queue();
+        private readonly Hashtable Closed = new Hashtable();
+
+        public delegate double GetDist(Node n1, Node n2);
+        //public GetDist Dist;
+
+        public delegate Node GetNodeByXY(int x, int y);
+        //public GetNodeByXY NodeXY;
+
+        public Node FindPath(Node s, Node f, GetDist Dist, GetNodeByXY NodeXY)
+        {
+            //Some checks
+            if ((Dist == null) | (NodeXY == null))
+            {
+                throw new ArgumentException("Delegate Function(s) not set! (Functions Dist and NodeXY)");
+            }
+
+            //Reset
+            Open.Clear();
+            Closed.Clear();
+            
+            //Aux
+            int[] dx = new int[8] { -1, -1, -1, 1, 1, 1, 0, 0 };
+            int[] dy = new int[8] { -1, 0, 1, -1, 0, 1, -1, 1 };
+            Node n, nn;
+
+            //GO!
+            //s.g = 0  // s is the start node
+            s.gdist = 0;
+            //s.h = GoalDistEstimate( s )
+            s.hdist = Dist(s, f);
+            //s.f = s.g + s.h
+            //OK
+            //s.Parent = null
+            s.Parent = null;
+            //push s on Open
+            Open.Add(s, null);
+            //while Open is not empty
+            while (Open.Count > 0)
+            {
+                //pop node n from Open  // n has the lowest f
+                //System.Windows.Forms.MessageBox.Show(Open.Count.ToString());
+                n = Open.Pop();
+                //if n is a goal node
+                if (n.Equals(f))
+                {
+                    //					construct path
+                    //					return success
+                    return n;
+                }
+                //for each successor n' of n
+                for (int i = 0; i <= 7; i++)
+                {
+                    nn = NodeXY(n.px + dx[i], n.py + dy[i]);
+                    if (nn != null)
+                    {
+                        //newg = n.g + cost(n,n')
+                        double newg = n.gdist + nn.cost;
+                        //if n' is in Open or Closed,and n'.g <= newg {	skip }
+                        if (!((Open.Contains(nn) || Closed.Contains(nn)) && (nn.gdist <= newg)))
+                        {
+                            //n'.Parent = n
+                            nn.Parent = n;
+                            //n'.g = newg
+                            nn.gdist = newg;
+                            //n'.h = GoalDistEstimate( n' )
+                            nn.hdist = Dist(nn, f);
+                            //n'.f = n'.g + n'.h
+                            //OK
+                            //if n' is in Closed	remove it from Closed
+                            //nn.visited=false;
+                            if (Closed.Contains(nn)) Closed.Remove(nn);
+                            //if n' is not yet in Open  push n' on Open
+                            if (!(Open.Contains(nn))) Open.Add(nn, null);
+                        }
+                    }
+                }
+                //push n onto Closed
+                //n.visited=true;
+                Closed.Add(n, null);
+            }
+            //			return failure // if no path found			
+            return null;
+        }
+    }
+}
