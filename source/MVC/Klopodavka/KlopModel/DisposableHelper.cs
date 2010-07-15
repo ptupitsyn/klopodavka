be19b790cb6a5841ac5d8fc82a9d72b8bf1d9981
nAXAH1 <nAXAH1@36d63d38-9f94-11de-a33f-1de61d9b9616>
2010-07-15 12:08:03 +0000

    Defer Available property change

---
+++ source/MVC/Klopodavka/KlopModel/DisposableHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace KlopModel
+{
+   internal class DisposableHelper : IDisposable
+   {
+      #region Fields and Constants
+
+      private readonly Action end;
+
+      #endregion
+
+      #region Constructors
+
+      public DisposableHelper(Action end)
+      {
+         this.end = end;
+      }
+
+
+      public DisposableHelper(Action begin, Action end)
+      {
+         this.end = end;
+         begin();
+      }
+
+      #endregion
+
+      #region Public methods
+
+      /// <summary>
+      /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+      /// </summary>
+      public void Dispose()
+      {
+         end();
+      }
+
+      #endregion
+   }
+}
