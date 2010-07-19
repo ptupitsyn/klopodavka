49f7fc80584ec99cb73975e1bda37ef1f40bdcf0
nAXAH1 <nAXAH1@36d63d38-9f94-11de-a33f-1de61d9b9616>
2010-07-19 14:51:22 +0000

    PriorityQueue fixed. SortedSet is not applicable.

--- source/MVC/Klopodavka/KlopAi/algo/PriorityQueue.cs
+++ source/MVC/Klopodavka/KlopAi/algo/PriorityQueue.cs
@@ -7,7 +7,7 @@ namespace KlopAi.algo
    /// <summary>
    /// Summary description for priority_queue.
    /// </summary>
-   public class PriorityQueue : SortedSet<Node>
+   public class PriorityQueue : HashSet<Node>
    {
       #region Nested type: NodeComparer
 
@@ -53,7 +53,7 @@ namespace KlopAi.algo
       /// <summary>
       /// Initializes a new instance of the <see cref="PriorityQueue"/> class.
       /// </summary>
-      public PriorityQueue() : base(NodeComparer.Instance)
+      public PriorityQueue() // : base(NodeComparer.Instance)
       {
       }
 
@@ -65,9 +65,9 @@ namespace KlopAi.algo
       /// Get the lowest element in queue.
       /// OLD VERSION.
       /// </summary>
-      public Node Pop1()
+      public Node Pop()
       {
-         var o = new Node(); // lowest object in queue
+         Node o = null; // lowest object in queue
          var d = double.MaxValue;
 
          foreach (Node n in this)
@@ -87,8 +87,9 @@ namespace KlopAi.algo
       /// Get the lowest element in queue.
       /// </summary>
       /// <returns></returns>
-      public Node Pop()
+      public Node Pop1()
       {
+         // Works when derived from SortedSet. However, seems to work incorrect.
          var node = this.Last();
          Remove(node);
          return node;
