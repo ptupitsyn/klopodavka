962b8b6cd494e2d835619594801a52d6d9c510d9
naxah1 <naxah1@36d63d38-9f94-11de-a33f-1de61d9b9616>
2011-09-04 18:01:58 +0000

    MainViewModel, reorganizing views and models

--- source/KlopViewWpf/ViewModels/MainViewModel.cs
+++ source/KlopViewWpf/ViewModels/MainViewModel.cs
@@ -1,6 +1,53 @@
-﻿namespace KlopViewWpf.ViewModels
+﻿#region Usings
+
+using GalaSoft.MvvmLight;
+
+#endregion
+
+namespace KlopViewWpf.ViewModels
 {
-   public class MainViewModel
+   public class MainViewModel : ViewModelBase
    {
+      #region Fields and Constants
+
+      private KlopGameViewModel _gameViewModel;
+
+      private bool _isMenuVisible;
+
+      #endregion
+
+      #region Constructors
+
+      public MainViewModel()
+      {
+         GameViewModel = new KlopGameViewModel();
+         IsMenuVisible = true;
+      }
+
+      #endregion
+
+      #region Public properties and indexers
+
+      public KlopGameViewModel GameViewModel
+      {
+         get { return _gameViewModel; }
+         private set
+         {
+            _gameViewModel = value;
+            RaisePropertyChanged("GameViewModel");
+         }
+      }
+
+      public bool IsMenuVisible
+      {
+         get { return _isMenuVisible; }
+         private set
+         {
+            _isMenuVisible = value;
+            RaisePropertyChanged("IsMenuVisible");
+         }
+      }
+
+      #endregion
    }
 }
---
+++ source/KlopViewWpf/Views/GameField.xaml.cs
@@ -0,0 +1,60 @@
+﻿#region Usings
+
+using System.Windows.Input;
+using KlopViewWpf.ViewModels;
+
+#endregion
+
+namespace KlopViewWpf.Views
+{
+   /// <summary>
+   /// Interaction logic for GameField.xaml
+   /// </summary>
+   public partial class GameField
+   {
+      #region Constructors
+
+      public GameField()
+      {
+         InitializeComponent();
+      }
+
+      #endregion
+
+      #region Public properties and indexers
+
+      public KlopGameViewModel ViewModel
+      {
+         get { return (KlopGameViewModel) DataContext; }
+      }
+
+      #endregion
+
+      #region Event handlers
+
+      /// <summary>
+      /// Handles the MouseLeftButtonUp event of the KlopCell control.
+      /// </summary>
+      /// <param name="sender">The source of the event.</param>
+      /// <param name="e">The <see cref="System.Windows.Input.MouseButtonEventArgs"/> instance containing the event data.</param>
+      private void KlopCell_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+      {
+         if (ViewModel.Model.CurrentPlayer.Human)
+         {
+            ViewModel.MakeTurnCommand.Execute(((KlopCell2) sender).Cell);
+         }
+      }
+
+      /// <summary>
+      /// Handles the MouseEnter event of the KlopCell control.
+      /// </summary>
+      /// <param name="sender">The source of the event.</param>
+      /// <param name="e">The <see cref="System.Windows.Input.MouseEventArgs"/> instance containing the event data.</param>
+      private void KlopCell_MouseEnter(object sender, MouseEventArgs e)
+      {
+         ViewModel.SetActiveCellCommand.Execute(((KlopCell2) sender).Cell);
+      }
+
+      #endregion
+   }
+}
