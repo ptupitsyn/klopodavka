74777b13c6d264962cd5a72b348076f31dc788b6
naxah1 <naxah1@36d63d38-9f94-11de-a33f-1de61d9b9616>
2011-11-09 20:29:41 +0000

    Increase AI Aggression (attack threshold) - seems to be good!!

--- source/KlopAi/KlopAiPlayer.cs
+++ source/KlopAi/KlopAiPlayer.cs
@@ -19,7 +19,7 @@ namespace KlopAi
    {
       #region Fields and Constants
 
-      private const decimal AttackThreshold = 0.3M;
+      private const decimal AttackThreshold = 0.5M;
       private readonly object _syncRoot = new object();
       private int[,] _distanceMap;
       private IKlopModel _model;
