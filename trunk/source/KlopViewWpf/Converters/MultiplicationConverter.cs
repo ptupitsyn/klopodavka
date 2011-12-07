000b5fa421f7d6bb33bde0139d5b14b40f8eaa3a
nAXAH1 <nAXAH1@36d63d38-9f94-11de-a33f-1de61d9b9616>
2011-12-07 07:25:36 +0000

    PlayerInfo resize fix

---
+++ trunk/source/KlopViewWpf/Converters/MultiplicationConverter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+
+namespace KlopViewWpf.Converters
+{
+   /// <summary>
+   /// Multiplies value by parameter. Works with Double.
+   /// </summary>
+   public class MultiplicationConverter : IValueConverter
+   {
+      #region IValueConverter implementation
+
+      public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+      {
+         if (!(value is double)) return 0;
+         var val = (double) value;
+         if (parameter is int)
+         {
+            return val*((int) parameter);
+         }
+         if (parameter is double)
+         {
+            return val*((double) parameter);
+         }
+         double par;
+         if (parameter is string && double.TryParse((string)parameter, out par))
+         {
+            return val * par;
+         }
+
+         return val;
+      }
+
+
+      public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+      {
+         if (!(value is double)) return 0;
+         var val = (double) value;
+         if (parameter is int)
+         {
+            return val/((int) parameter);
+         }
+         if (parameter is double)
+         {
+            return val/((double) parameter);
+         }
+         double par;
+         if (parameter is string && double.TryParse((string)parameter, out par))
+         {
+            return val / par;
+         }
+
+         return val;
+      }
+
+      #endregion
+   }
+}
