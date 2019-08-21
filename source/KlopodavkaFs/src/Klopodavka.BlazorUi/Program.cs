66e864f38de851afd252499a346f772ba37f7942
Pavel Tupitsyn <ptupitsyn@apache.org>
2019-08-21 14:59:35 +0300

    Adding domain model

---
+++ source/KlopodavkaFs/src/Klopodavka.BlazorUi/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Klopodavka.Game;
+
+namespace Klopodavka.BlazorUi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Player("Player 1", 1);
+            Console.WriteLine(p);
+        }
+    }
+}
