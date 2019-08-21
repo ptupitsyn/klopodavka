b082725872db3dddfa10fe8fda4781ed2f01e927
Pavel Tupitsyn <ptupitsyn@apache.org>
2019-08-21 18:31:53 +0300

    Implement moves detection

--- source/KlopodavkaFs/src/Klopodavka.Game/Board.fs
+++ source/KlopodavkaFs/src/Klopodavka.Game/Board.fs
@@ -35,15 +35,35 @@ let createBoard =
     arr |> Tiles
 
 let neighbors board x y =
-    let offs = [-1; 0; 1]
-    let (w, h) = size board
+    let offs = [ -1; 0; 1 ]
+    let w, h = size board
     Seq.allPairs offs offs
-    |> Seq.except [(0, 0)]
+    |> Seq.except [ (0, 0) ]
     |> Seq.map (fun (a, b) -> (x + a, y + b))
     |> Seq.where (fun (a, b) -> a >= 0 && b >= 0 && a < w && b < h)
 
 let moves board player =
     let w, h = size board
     let bx, by = basePosition player w h
-    [(bx, by)]
+    let visited = Array2D.create w h false
+
+    let rec loop (tiles: seq<int * int>) = seq {
+        for pos in tiles do
+            let x, y = pos
+            match visited.[x, y] with
+            | true -> ()
+            | false ->
+                visited.[x, y] <- true
+                let t = tile board x y
+                let loopNeighbors () = loop (neighbors board x y)
+
+                match t with
+                | Empty -> yield pos
+                | Alive p when p = player -> yield! loopNeighbors()
+                | Squashed p when p = player -> yield! loopNeighbors()
+                | _ -> ()
+
+    }
+
+    loop [bx, by]
 
