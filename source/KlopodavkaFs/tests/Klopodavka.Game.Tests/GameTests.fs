efffd64be087b3008f66c29c2ca440dfb0be205e
Pavel Tupitsyn <ptupitsyn@apache.org>
2019-08-21 16:15:37 +0300

    Add board tests

--- source/KlopodavkaFs/tests/Klopodavka.Game.Tests/GameTests.fs
+++ source/KlopodavkaFs/tests/Klopodavka.Game.Tests/GameTests.fs
@@ -8,17 +8,17 @@ module GameTests =
     let tests =
         testList "Game module tests" [
 
-            testCase "createGame returns board of correct size" <| fun _ ->
+            testCase "createBoard returns board of correct size" <| fun _ ->
                 let board = Game.createBoard
                 Expect.equal (board.GetLength 0) Game.BoardWidth "Board width is correct"
                 Expect.equal (board.GetLength 1) Game.BoardHeight "Board height is correct"
 
-            testCase "createGame returns board with base tiles" <| fun _ ->
+            testCase "createBoard returns board with base tiles" <| fun _ ->
                 let board = Game.createBoard
                 Expect.equal board.[29, 5] (Base Red) "Red base is at 29, 5"
                 Expect.equal board.[5, 35] (Base Blue) "Blue base is at 5, 35"
 
-            testCase "createGame returns board with empty tiles except bases" <| fun _ ->
+            testCase "createBoard returns board with empty tiles except bases" <| fun _ ->
                 let board = Game.createBoard
                 let tiles =
                     Seq.allPairs (seq {0 .. Game.BoardWidth - 1}) (seq {0 .. Game.BoardHeight - 1})
