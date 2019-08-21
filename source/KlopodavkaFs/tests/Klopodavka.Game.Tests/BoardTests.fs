5a57fca70404c445bc8afd595f4b5bf588a9f93d
Pavel Tupitsyn <ptupitsyn@apache.org>
2019-08-21 17:41:07 +0300

    Implement neighbors

--- source/KlopodavkaFs/tests/Klopodavka.Game.Tests/BoardTests.fs
+++ source/KlopodavkaFs/tests/Klopodavka.Game.Tests/BoardTests.fs
@@ -26,4 +26,19 @@ let tests =
                 |> Seq.except [Base Blue; Base Red]
             Expect.allEqual tiles Empty "All tiles are empty except bases"
 
+        testCase "neighbors returns 3 tiles for top left corner" <| fun _ ->
+            let board = createBoard
+            let res = neighbors board 0 0
+            Expect.sequenceEqual res [(0, 1); (1, 0); (1, 1)] "3 cells in the corner"
+
+        testCase "neighbors returns 5 tiles for sides" <| fun _ ->
+            let board = createBoard
+            let res = neighbors board (DefaultWidth - 1) 10
+            Expect.sequenceEqual res [(32, 9); (32, 10); (32, 11); (33, 9); (33, 11)] "6 cells on the right side"
+
+        testCase "neighbors returns 9 tiles for mid field" <| fun _ ->
+            let board = createBoard
+            let res = neighbors board 5 6
+            Expect.sequenceEqual res [(4, 5); (4, 6); (4, 7); (5, 5); (5, 7); (6, 5); (6, 6); (6, 7)] "9 cells in the middle"
+
     ]
