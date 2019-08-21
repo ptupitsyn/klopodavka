3cf6bf9843b4e422cd029a11ac4e03460e2db2e7
Pavel Tupitsyn <ptupitsyn@apache.org>
2019-08-21 17:56:06 +0300

    Refactor board sizing

--- source/KlopodavkaFs/src/Klopodavka.Game/Board.fs
+++ source/KlopodavkaFs/src/Klopodavka.Game/Board.fs
@@ -10,7 +10,15 @@ let DefaultHeight = 40
 [<Literal>]
 let BaseOffset = 5
 
-let getBasePos player width height =
+let tiles (Tiles board) = board
+
+let tile board x y = (tiles board).[x, y]
+
+let size board =
+    let arr = tiles board
+    (arr.GetLength(0), arr.GetLength(1))
+
+let basePos player width height =
     match player with
         | Red -> (width - BaseOffset, BaseOffset)
         | Blue -> (BaseOffset, height - BaseOffset)
@@ -18,27 +26,24 @@ let getBasePos player width height =
 let createBoard =
     let arr = Array2D.create DefaultWidth DefaultHeight Empty
 
-    let redX, redY = getBasePos Red DefaultWidth DefaultHeight
+    let redX, redY = basePos Red DefaultWidth DefaultHeight
     arr.[redX, redY] <- Base Red
 
-    let blueX, blueY = getBasePos Blue DefaultWidth DefaultHeight
+    let blueX, blueY = basePos Blue DefaultWidth DefaultHeight
     arr.[blueX, blueY] <- Base Blue
 
     arr |> Tiles
 
-let tiles (Tiles board) = board
-
-let tile board x y = (tiles board).[x, y]
-
-let width board = (tiles board).GetLength(0)
-
-let height board = (tiles board).GetLength(1)
-
 let neighbors board x y =
     let offs = [-1; 0; 1]
+    let (w, h) = size board
     Seq.allPairs offs offs
     |> Seq.except [(0, 0)]
     |> Seq.map (fun (a, b) -> (x + a, y + b))
-    |> Seq.where (fun (a, b) -> a >= 0 && b >= 0 && a < width board && b < height board)
+    |> Seq.where (fun (a, b) -> a >= 0 && b >= 0 && a < w && b < h)
 
+let moves board player =
+    let w, h = size board
+    let bx, by = basePos player w h
+    [(bx, by)]
 
--- source/KlopodavkaFs/tests/Klopodavka.Game.Tests/BoardTests.fs
+++ source/KlopodavkaFs/tests/Klopodavka.Game.Tests/BoardTests.fs
@@ -10,8 +10,9 @@ let tests =
 
         testCase "createBoard returns board of correct size" <| fun _ ->
             let board = createBoard
-            Expect.equal (width board) DefaultWidth "Board width is default"
-            Expect.equal (height board) DefaultHeight "Board height is default"
+            let w, h = size board
+            Expect.equal w DefaultWidth "Board width is default"
+            Expect.equal h DefaultHeight "Board height is default"
 
         testCase "createBoard returns board with base tiles" <| fun _ ->
             let board = createBoard
@@ -20,8 +21,9 @@ let tests =
 
         testCase "createBoard returns board with empty tiles except bases" <| fun _ ->
             let board = createBoard
+            let w, h = size board
             let tiles =
-                Seq.allPairs (seq {0 .. width board - 1}) (seq {0 .. height board - 1})
+                Seq.allPairs (seq {0 .. w - 1}) (seq {0 .. h - 1})
                 |> Seq.map (fun (x, y) -> tile board x y)
                 |> Seq.except [Base Blue; Base Red]
             Expect.allEqual tiles Empty "All tiles are empty except bases"
